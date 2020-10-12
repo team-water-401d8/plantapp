1a656340077639c9ef8a7e3ae87e542f11c46e76
Chandler Puckett <chandler.puckett1@gmail.com>
2020-10-12 10:28:59 -0700

    initial homepage

--- plantApp/src/main/java/com/teamwater/plantApp/controllers/HomeController.java
+++ plantApp/src/main/java/com/teamwater/plantApp/controllers/HomeController.java
@@ -1,4 +1,17 @@
 package com.teamwater.plantApp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String homePage(Principal principal, Model m){
+        m.addAttribute("user", principal);
+        return "home";
+    }
 }

adabb012b28c03a4a87cf6070236af32a416cb80
Chandler Puckett <chandler.puckett1@gmail.com>
2020-10-13 16:40:32 -0700

    used jQuery to pull form data & prevent reload

--- plantApp/src/main/resources/static/public/search.js
+++ plantApp/src/main/resources/static/public/search.js
@@ -9,7 +9,14 @@ $("#searchButton").click(function() {
 $(".addPlant").on("submit",function(event){
     event.preventDefault();
 
+    $.post("/addPlantToGarden",
+        {
+            common_name: $(".common_name").attr("value"),
+            image_url: $(".image_url").attr("value")
+        }
+    );
+
    // jQuery post request goes to route
    // do all the things
-    console.log("trying to prevent reload")
+    console.log("----- prevent reload ------")
 });

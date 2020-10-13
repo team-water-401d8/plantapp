1173385a72b505450d78e30fd285cf4432aacf21
Chandler Puckett <chandler.puckett1@gmail.com>
2020-10-13 15:45:52 -0700

    working on jquery stuff

--- plantApp/src/main/java/com/teamwater/plantApp/controllers/PlantController.java
+++ plantApp/src/main/java/com/teamwater/plantApp/controllers/PlantController.java
@@ -1,8 +1,18 @@
 package com.teamwater.plantApp.controllers;
 
+import com.nimbusds.oauth2.sdk.http.HTTPRequest;
 import com.teamwater.plantApp.models.plant.PlantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @Controller
 public class PlantController {
@@ -10,7 +20,17 @@ public class PlantController {
     @Autowired
     PlantRepository plantRepository;
 
+//  ======== routes =======
+    @PostMapping("/addPlantToGarden")
+    @ResponseStatus(value= HttpStatus.OK)
+    public void addPlantToGarden(@RequestParam(value="common_name")String common_name,
+                                         @RequestParam(value="image_url")String image_url){
 
+        System.out.println(common_name);
+        System.out.println(image_url);
 
+        System.out.println("------ added PLANT TO DB ------");
+
+    }
 
 }
--- plantApp/src/main/resources/static/public/search.js
+++ plantApp/src/main/resources/static/public/search.js
@@ -4,4 +4,12 @@ $("#searchButton").click(function() {
     if (query) {
             window.location.href = `/search?q=${query}`
     }
-})
+});
+
+$(".addPlant").on("submit",function(event){
+    event.preventDefault();
+
+   // jQuery post request goes to route
+   // do all the things
+    console.log("trying to prevent reload")
+});

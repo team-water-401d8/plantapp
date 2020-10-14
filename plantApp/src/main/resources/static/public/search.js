eff68c66b9990729a573a752b6d60d7f9a30660a
Chandler Puckett <chandler.puckett1@gmail.com>
2020-10-14 14:48:45 -0700

    boostrap styling home page with carousel and 3 images

--- plantApp/src/main/resources/static/public/search.js
+++ plantApp/src/main/resources/static/public/search.js
@@ -12,28 +12,13 @@ $(".addPlant").on("submit",function(event){
 
     $.post("/addPlantToGarden",                 // post data to /addPlantToGarden
         {
-<<<<<<< HEAD
             common_name: $(this).children("h2").text(),
             image_url: $(this).children("img").attr("src")
         }
     );
-
-    console.log( $(this).children("h2").text() );
-    console.log( $(this).children("img").attr("src") );
-=======
-
-        //need to add id to store here and on the controller
-
-            common_name: $(".common_name").attr("value"),
-            image_url: $(".image_url").attr("value")
-        }
-    );
-
-
    // jQuery post request goes to route
    // do all the things
     console.log("----- prevent reload ------")
->>>>>>> 4e87aa583f3b3924f6e151cc9d6db0f9924ee0b9
 });
 
 //TODO: We'll need to add the "add to garden button" on the details.html
